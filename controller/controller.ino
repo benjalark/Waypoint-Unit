4d100a7e9127405f5cd10f3a67d904f08ccac9ba
benjalark <bsjlarkin@proton.me>
2026-04-23 15:35:36 -0400

    added framework for a DHT sensor, shortened no. features to 6

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,69 +1,85 @@
-const int MAX_POT_VAL = 1023; // max value of the potentiometer
-const int outPins[8] = {2,3,4,5,6,7,8,9};  // pins to be used for output
+#include <dht_nonblocking.h>
+
+const int NUM_PINS = 6;
+const int MAX_POT_VAL = 1023;
+
+const int outPins[NUM_PINS] = {2,3,4,5,6,7};
+const int inPins[NUM_PINS]  = {A0,A1,A2,A3,A4,A5};
+
+DHT_nonblocking dht_sensor(inPins[1], DHT_TYPE_11);
 
 void setup() {
-  Serial.begin(9600); // init signal processing speed in baud
-  for(int i = 0; i < 8; i++){ // init output pins
+  Serial.begin(9600);
+
+  for(int i = 0; i < NUM_PINS; i++){
     pinMode(outPins[i], OUTPUT);
   }
-
 }
 
 void loop() {
-  int potValue = analogRead(A0); // reading A0 pin for a potentiometer signal
-  int feature = (potValue * 8) / (MAX_POT_VAL + 1) + 1; // sections off signal into regions
+  static float temperature;
+  static float humidity;
 
+  // ALWAYS service DHT (critical)
+  bool dht_ready = measure_environment(&temperature, &humidity);
+
+  // read potentiometer
+  int potValue = analogRead(inPins[0]);
+  int feature = (potValue * NUM_PINS) / (MAX_POT_VAL + 1); // 0–5
+
+  // turn everything off
   off();
 
-  switch(feature){ // feature implementation (simple console log for the meantime)
+  // feature selection
+  switch(feature){
+    case 0:
+      /*digitalWrite(outPins[0], HIGH);
+
+      if (dht_ready) {
+        Serial.print("T = ");
+        Serial.print(temperature, 1);
+        Serial.print(" C, H = ");
+        Serial.print(humidity, 1);
+        Serial.println("%");
+      }
+      */
+      debug(potValue, feature);
+      break;
+
     case 1:
-      off();
-      digitalWrite(outPins[0], HIGH);
+      //digitalWrite(outPins[1], HIGH);
       debug(potValue, feature);
       break;
+
     case 2:
-      off();
-      digitalWrite(outPins[1], HIGH);
+      //digitalWrite(outPins[2], HIGH);
       debug(potValue, feature);
       break;
+
     case 3:
-      off();
-      digitalWrite(outPins[2], HIGH);
+      //digitalWrite(outPins[3], HIGH);
       debug(potValue, feature);
       break;
+
     case 4:
-      off();
-      digitalWrite(outPins[3], HIGH);
+      //digitalWrite(outPins[4], HIGH);
       debug(potValue, feature);
       break;
+
     case 5:
-      off();
-      digitalWrite(outPins[4], HIGH);
+      //digitalWrite(outPins[5], HIGH);
       debug(potValue, feature);
       break;
-    case 6:
-      off();
-      digitalWrite(outPins[5], HIGH);
-      debug(potValue, feature);
-      break;
-    case 7:
-      off();
-      digitalWrite(outPins[6], HIGH);
-      debug(potValue, feature);
-      break;
-    case 8:
-      off();
-      digitalWrite(outPins[7], HIGH);
-      debug(potValue, feature);
-      break;
+
     default:
-      Serial.print("Serious hardware malfunction. Check potentiometer");
+      Serial.println("Invalid state");
   }
-  
+
+  delay(50); // small stability delay
 }
 void off(){
   // turn all pins off
-  for(int i = 0; i < 8; i++){
+  for(int i = 0; i < NUM_PINS; i++){
     digitalWrite(outPins[i], LOW);
   }
 }
@@ -72,4 +88,17 @@ void debug(int i, int j){
   Serial.print(i);
   Serial.print(" Region: ");
   Serial.println(j);
+}
+// DHT11 (feature 1)
+static bool measure_environment(float *temperature, float *humidity)
+{
+  static unsigned long measurement_timestamp = 0;
+
+  // always call measure() to advance state machine
+  bool ready = dht_sensor.measure(temperature, humidity);
+  if (ready) {
+    measurement_timestamp = millis();
+    return true;
+  }
+  return false;
 }
