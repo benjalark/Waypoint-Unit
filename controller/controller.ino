6794c897a0afe6c1c87fcf03fc8af7b46af306e9
benjalark <bsjlarkin@proton.me>
2026-04-23 00:21:10 -0400

    initial hardware test, feature 1 with LED

--- controller/controller.ino
+++ controller/controller.ino
@@ -13,40 +13,51 @@ void loop() {
   int potValue = analogRead(A0); // reading A0 pin for a potentiometer signal
   int feature = (potValue * 8) / (MAX_POT_VAL + 1) + 1; // sections off signal into regions
 
-  // turn all pins off first
-  for(int i = 0; i < 8; i++){
-    digitalWrite(outPins[i], LOW);
-  }
+  off();
 
   switch(feature){ // feature implementation (simple console log for the meantime)
     case 1:
-      debug(potValue, feature);
+      off();
+      digitalWrite(outPins[0], HIGH); // testing region one with hardware
       break;
     case 2:
+      off();
       debug(potValue, feature);
       break;
     case 3:
+      off();
       debug(potValue, feature);
       break;
     case 4:
+      off();
       debug(potValue, feature);
       break;
     case 5:
+      off();
       debug(potValue, feature);
       break;
     case 6:
+      off();
       debug(potValue, feature);
       break;
     case 7:
+      off();
       debug(potValue, feature);
       break;
     case 8:
+      off();
       debug(potValue, feature);
       break;
     default:
       Serial.print("Serious hardware malfunction. Check potentiometer");
   }
   
+}
+void off(){
+  // turn all pins off
+  for(int i = 0; i < 8; i++){
+    digitalWrite(outPins[i], LOW);
+  }
 }
 void debug(int i, int j){
   Serial.print("Value: ");
