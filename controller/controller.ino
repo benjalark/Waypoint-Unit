79b6fb6ae96a640f375e44079e1a7d31910a55e6
benjalark <bsjlarkin@proton.me>
2026-04-27 18:25:35 -0400

    added gps coordinate feature

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,10 +1,21 @@
 #include <dht_nonblocking.h>
 #include <LiquidCrystal.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+//initialize gps pins and baud rate 
+//for NEO-6m baud is set to default (9600)
+static const int RXPin = 6, TXPin = 7;
+static const uint32_t GPSBaud = 9600;
+// The TinyGPS++ object
+TinyGPSPlus gps;
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
 
 const int NUM_MODES = 5;
 const int MAX_POT_VAL = 1023;
@@ -15,6 +26,7 @@ DHT_nonblocking dht_sensor(dataPins[2], DHT_TYPE_11);
 
 void setup() {
   Serial.begin(9600);
+  ss.begin(GPSBaud);
   lcd.begin(16, 2);
   for(int i = 0; i < NUM_MODES; i++){
     pinMode(dataPins[i], INPUT);
@@ -61,7 +73,19 @@ void loop() {
       break;
 
     case 1:
-      debug(potValue, feature);
+      // This displays information every time a new sentence is correctly encoded from the neo-6m
+      while (ss.available() > 0){
+        gps.encode(ss.read());
+        if (gps.location.isUpdated()){
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Lat= "); 
+          lcd.print(gps.location.lat(), 6);
+          lcd.setCursor(0, 1);
+          lcd.print(" Long= "); 
+          lcd.println(gps.location.lng(), 6);
+        }
+      }
       break;
 
     case 2:
