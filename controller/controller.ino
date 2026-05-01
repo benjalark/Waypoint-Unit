5e7d2e9e5e01b851c5c0dc471bd8eb37bddd143b
benjalark <bsjlarkin@proton.me>
2026-05-01 13:10:28 -0400

    refactor to fix float display issue

--- controller/controller.ino
+++ controller/controller.ino
@@ -12,7 +12,7 @@ static const uint32_t GPSBaud = 9600;
 TinyGPSPlus gps;
 SoftwareSerial ss(RXPin, TXPin);
 
-// DHT11
+// DHT
 #define DHT_PIN 8
 DHT_nonblocking dht_sensor(DHT_PIN, DHT_TYPE_11);
 
@@ -20,22 +20,27 @@ DHT_nonblocking dht_sensor(DHT_PIN, DHT_TYPE_11);
 #define NUM_MODES 5
 #define POT_PIN A0
 
+// ---- Fixed-point storage ----
+int temp10 = 0;   // temperature ×10
+int hum10  = 0;   // humidity ×10
+
+long lat5 = 0;    // latitude ×1e5
+long lon5 = 0;    // longitude ×1e5
+
+int alt_m = 0;
+int alt_ft = 0;
+
+int speed_mps10 = 0;
+int speed_mph10 = 0;
+
 // State
-float temperature = 0, humidity = 0;
 int lastFeature = -1;
 unsigned long lastDHTUpdate = 0;
 
-void setup() {
-  Serial.begin(9600);
-  ss.begin(GPSBaud);
-  lcd.begin(16, 2);
-  lcd.print("Initializing...");
-}
-
-// Nice printing function
+// ---- LCD helper ----
 void print2(const char* l1, const char* l2) {
   lcd.setCursor(0, 0);
-  lcd.print("                "); // clear line
+  lcd.print("                ");
   lcd.setCursor(0, 0);
   lcd.print(l1);
 
@@ -45,85 +50,130 @@ void print2(const char* l1, const char* l2) {
   lcd.print(l2);
 }
 
-// DHT non-blocking read
+// ---- DHT update (convert once) ----
 bool updateDHT() {
-  if (dht_sensor.measure(&temperature, &humidity)) {
-    if (millis() - lastDHTUpdate > 3000ul) { // DHT11 ~1Hz safe
+  float t, h;
+  if (dht_sensor.measure(&t, &h)) {
+    if (millis() - lastDHTUpdate > 2000) {
       lastDHTUpdate = millis();
+
+      temp10 = (int)(t * 10 + 0.5);
+      hum10  = (int)(h * 10 + 0.5);
+
       return true;
     }
   }
   return false;
 }
 
+void setup() {
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+  lcd.begin(16, 2);
+  lcd.print("Initializing...");
+}
+
 void loop() {
-  // Always feed GPS parser (non-blocking)
+
+  // ---- Always feed GPS ----
   while (ss.available()) {
     gps.encode(ss.read());
   }
 
-  // Read mode selector
+  // ---- Convert GPS once ----
+  if (gps.location.isUpdated()) {
+    lat5 = (long)(gps.location.lat() * 100000);
+    lon5 = (long)(gps.location.lng() * 100000);
+  }
+
+  if (gps.altitude.isUpdated()) {
+    alt_m  = (int)gps.altitude.meters();
+    alt_ft = (int)gps.altitude.feet();
+  }
+
+  if (gps.speed.isUpdated()) {
+    speed_mps10 = (int)(gps.speed.mps() * 10 + 0.5);
+    speed_mph10 = (int)(gps.speed.mph() * 10 + 0.5);
+  }
+
+  // ---- Mode selection ----
   int feature = (analogRead(POT_PIN) * NUM_MODES) / 1024;
 
-  // Mode change handling
   if (feature != lastFeature) {
     lcd.clear();
     lastFeature = feature;
   }
 
+  // ---- Modes ----
   switch (feature) {
 
-    // MODE 0: TEMP + HUM
-    case 0:
+    // --- TEMP / HUM ---
+    case 0: {
       if (updateDHT()) {
         char l1[17], l2[17];
-        snprintf(l1, 17, "T: %.1f C", temperature);
-        snprintf(l2, 17, "H: %.1f %%", humidity);
+
+        snprintf(l1, 17, "T: %d.%d C", temp10/10, abs(temp10%10));
+        snprintf(l2, 17, "H: %d.%d %%", hum10/10, abs(hum10%10));
+
         print2(l1, l2);
       }
       break;
+    }
 
-    // MODE 1: LAT/LON
-    case 1:
-      if (gps.location.isUpdated()) {
-        char l1[17], l2[17];
-        snprintf(l1, 17, "Lat: %.4f", gps.location.lat());
-        snprintf(l2, 17, "Lon: %.4f", gps.location.lng());
-        print2(l1, l2);
-      }
+    // --- LAT / LON ---
+    case 1: {
+      char l1[17], l2[17];
+
+      snprintf(l1, 17, "Lat:%ld.%05ld",
+               lat5/100000, labs(lat5%100000));
+
+      snprintf(l2, 17, "Lon:%ld.%05ld",
+               lon5/100000, labs(lon5%100000));
+
+      print2(l1, l2);
       break;
+    }
 
-    // MODE 2: ALTITUDE
-    case 2:
-      if (gps.altitude.isUpdated()) {
-        char l1[17], l2[17];
-        snprintf(l1, 17, "Alt: %.0f m", gps.altitude.meters());
-        snprintf(l2, 17, "Alt: %.0f ft", gps.altitude.feet());
-        print2(l1, l2);
-      }
+    // --- ALTITUDE ---
+    case 2: {
+      char l1[17], l2[17];
+
+      snprintf(l1, 17, "Alt:%dm", alt_m);
+      snprintf(l2, 17, "Alt:%dft", alt_ft);
+
+      print2(l1, l2);
       break;
+    }
 
-    // MODE 3: DATE/TIME
-    case 3:
-      if (gps.time.isUpdated() && gps.date.isUpdated()) {
+    // --- DATE / TIME ---
+    case 3: {
+      if (gps.time.isValid() && gps.date.isValid()) {
         char l1[17], l2[17];
+
         snprintf(l1, 17, "%02d/%02d/%02d",
           gps.date.day(), gps.date.month(), gps.date.year() % 100);
+
         snprintf(l2, 17, "%02d:%02d:%02d",
           gps.time.hour(), gps.time.minute(), gps.time.second());
-        print2(l1, l2);
-      }
-      break;
 
-    // MODE 4: SPEED
-    case 4:
-      if (gps.speed.isUpdated()) {
-        char l1[17], l2[17];
-        snprintf(l1, 17, "mph: %.1f", gps.speed.mph());
-        snprintf(l2, 17, "m/s: %.1f", gps.speed.mps());
         print2(l1, l2);
       }
       break;
+    }
+
+    // --- SPEED ---
+    case 4: {
+      char l1[17], l2[17];
+
+      snprintf(l1, 17, "mph:%d.%d",
+               speed_mph10/10, abs(speed_mph10%10));
+
+      snprintf(l2, 17, "m/s:%d.%d",
+               speed_mps10/10, abs(speed_mps10%10));
+
+      print2(l1, l2);
+      break;
+    }
 
     default:
       print2("Invalid mode", "");
